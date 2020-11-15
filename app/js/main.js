1616e51f760e0cbf01a014049a497e5b0d4380fb
Artur Nasibullin <nasibullin.artur@gmail.com>
2020-11-15 13:09:26 +0500

    Создал блок модального окна

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
+function openModal () {
+	const modal = $(.)
+}
+
+
+
 
 $(function() {
 	let pricesItem = $('.prices__item');
@@ -16,15 +22,34 @@ $(function() {
 	}, function(){
 		catalogitem.removeClass('catalog-item--hover');
 	});
+
+	// Фиксированное меню после прокрутки
+	let header = $('.header-top__wrapper');
+	let mainPage = $('.header');
+	let mainPageH = mainPage.innerHeight();
+	let scrollPos = $(window).scrollTop();
+	
+	$(window).on('scroll load resize', function() {
+		let mainPageH = mainPage.innerHeight();
+		scrollPos = $(this).scrollTop();
+	
+		if (scrollPos > mainPageH) {
+		header.addClass('fixed');
+		} else {
+		header.removeClass('fixed');
+		}
+	});
 	
 });
 
 $(window).on('load', (function() {
+	// Плагин twentyTwenty
 	$(".before-after").twentytwenty({
 		before_label: 'Без скинали',
     	after_label: 'Со скинали',
 	});
 
+	// Слик слайдер
 	$('.before-slider').slick({
 		draggable: false,
 		swipe: false,
@@ -34,16 +59,29 @@ $(window).on('load', (function() {
 		nextArrow: $('.arrow-right')
 	});
 
+	// Показать меню
 	$('.header-top__nav-btn').on('click', function(){
-		$('.nav-bar__content').toggleClass('nav-bar__content--active');
+		$('.nav-bar').toggleClass('nav-bar--active');
 	});
 
-	$('a[href^="#"]').click(function() {
-		let _href = $(this).attr('href');
-		$('html,body').animate({ scrollTop: $(_href).offset().top -50 + 'px'});
-		return false;
+	  // Smooth scroll
+	$('[data-scroll]').on('click', function(event){
+		event.preventDefault();
+
+		let elementID = $(this).data('scroll');
+		let elementOffset = $(elementID).offset().top;
+		
+
+		$('html, body').animate({
+		scrollTop: elementOffset - 80
+		}, 1000);
+
+
+		$('.nav-bar').removeClass('nav-bar--active');
+		
 	});
 
   }));
 
+  
 
